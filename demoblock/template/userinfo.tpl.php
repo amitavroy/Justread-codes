9afe5c648e3bffa51c3e558ad81315cc2993ff26
Amitav Roy - Dell <reachme@amitavroy.com>
2012-06-20 08:35:11 +0530

    Added the module for the new tutorial.
    Signed-off-by: Amitav Roy - Dell <reachme@amitavroy.com>

---
+++ demoblock/template/userinfo.tpl.php
@@ -0,0 +1,5 @@
+<p>Welcome <strong><?php print $username; ?></strong></p>
+
+<p><strong>Your email:</strong><?php print $email; ?></p>
+
+<p><strong>Timezone that you are following:</strong> <?php print $timezone; ?></p>
